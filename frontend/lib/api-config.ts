44834f54dc8f85477501326155bdff4059f964f1
Hassansito <108918273+Hassansito@users.noreply.github.com>
2025-09-15 01:13:47 -0400

    Update api-config.ts

--- frontend/lib/api-config.ts
+++ frontend/lib/api-config.ts
@@ -1,6 +1,5 @@
 export const API_CONFIG = {
-  //BASE_URL: process.env.NEXT_PUBLIC_API_URL || "https://apicp.acubamos.cu/api",
-  BASE_URL: "https://apicp.acubamos.cu/api",
+  BASE_URL: process.env.NEXT_PUBLIC_API_URL || "https://apicp.acubamos.cu/api",
   TIMEOUT: 10000,
   ENDPOINTS: {
     ENTRIES: "/entries",
@@ -20,4 +19,5 @@ export const getAuthHeaders = () => {
         "Content-Type": "application/json",
       }
 }
+
 
